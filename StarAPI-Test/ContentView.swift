4890a8857dab4b6601024254be8739541cb426a7
Maani2594-M <manikanteshiosdev@gmail.com>
2023-11-06 21:18:44 -0600

    Starter Project

---
+++ StarAPI-Test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StarAPI-Test
+//
+//  Created by Mani V on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarAPI-Test/StarAPI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarAPI_TestApp.swift
+//  StarAPI-Test
+//
+//  Created by Mani V on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct StarAPI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
