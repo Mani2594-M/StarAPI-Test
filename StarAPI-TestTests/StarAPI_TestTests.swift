b0bc51eb921ee687d1c3f8cde9a82b2786ef597a
Maani2594-M <manikanteshiosdev@gmail.com>
2023-11-06 22:52:30 -0600

    add test cases

--- StarAPI-TestTests/StarAPI_TestTests.swift
+++ StarAPI-TestTests/StarAPI_TestTests.swift
@@ -6,16 +6,23 @@
 //
 
 import XCTest
+import Combine
+
 @testable import StarAPI_Test
 
 final class StarAPI_TestTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var subscriptions = Set<AnyCancellable>()
+    var sut: DownloadAPIClient!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        sut = DownloadAPIClient()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
     }
 
     func testExample() throws {
@@ -28,9 +35,80 @@ final class StarAPI_TestTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func test_launch_success() {
+        
+        let launch = Result<[Launch], APIError>.success([Launch.testLaunch1()])
+        let launches = LaunchFetcherViewModel(service: MockAPIService(launch: launch))
+        
+        let promise = expectation(description: "Launches successful")
+        
+        launches.$launches.sink { launches in
+            if launches.count > 0 {
+                promise.fulfill()
+            }
+        }.store(in: &subscriptions)
+        
+        wait(for: [promise], timeout: 2)
+    }
+    
+    func test_loading_error() {
+        
+        let launch = Result<[Launch], APIError>.failure(APIError.badUrl)
+        let launcher = LaunchFetcherViewModel(service: MockAPIService(launch: launch))
+        
+        let promise = expectation(description: "Launch Unsuccessful")
+        
+        launcher.$launches.sink { launches in
+            if !launches.isEmpty {
+                promise.fulfill()
+            }
+        }.store(in: &subscriptions)
+        
+        launcher.$errorMessage.sink { message in
+            if message != nil {
+                promise.fulfill()
+            }
+        }.store(in: &subscriptions)
+        
+        wait(for: [promise], timeout: 2)
+    }
+    
+    func test_UsesExpectedHost() {
+        
+        let url = "https://urldefense.proofpoint.com/v2/url?u=https-3A__api.spacexdata.com_v3_launches&d=DwIGAg&c=BioHiDP8cpFFEOWoiyRJQw&r=3FaZC4i26BbLvHTIgl3oS-4rsIuvBI95wqEzuFbXR5I&m=kylOGjUVRcexWeN1YBwN081G5IGWlqWE008QHRdMNR2zDcyh9IM2MWJoBVPVYXX0&s=ZBuZiCyB2zc0Z7m2CcpPQDNGmvM3IOO7B_OLsuHJoEk&e="
+        sut.downloadData(withUrl: url)
+        guard let url1 = URL(string: url) else {
+            XCTFail(); return
+        }
+        let urlComponents = URLComponents(url: url1, resolvingAgainstBaseURL: true)
+        XCTAssertEqual(urlComponents?.host, "urldefense.proofpoint.com")
+    }
+    
+    func test_UsesExpectedPath() {
+        
+        let url = "https://urldefense.proofpoint.com/v2/url?u=https-3A__api.spacexdata.com_v3_launches&d=DwIGAg&c=BioHiDP8cpFFEOWoiyRJQw&r=3FaZC4i26BbLvHTIgl3oS-4rsIuvBI95wqEzuFbXR5I&m=kylOGjUVRcexWeN1YBwN081G5IGWlqWE008QHRdMNR2zDcyh9IM2MWJoBVPVYXX0&s=ZBuZiCyB2zc0Z7m2CcpPQDNGmvM3IOO7B_OLsuHJoEk&e="
+        sut.downloadData(withUrl: url)
+        guard let url1 = URL(string: "https://urldefense.proofpoint.com/v2/url?u=https-3A__api.spacexdata.com_v3_launches&d=DwIGAg&c=BioHiDP8cpFFEOWoiyRJQw&r=3FaZC4i26BbLvHTIgl3oS-4rsIuvBI95wqEzuFbXR5I&m=kylOGjUVRcexWeN1YBwN081G5IGWlqWE008QHRdMNR2zDcyh9IM2MWJoBVPVYXX0&s=ZBuZiCyB2zc0Z7m2CcpPQDNGmvM3IOO7B_OLsuHJoEk&e=") else {
+            XCTFail(); return
+        }
+        let urlComponents = URLComponents(url: url1, resolvingAgainstBaseURL: true)
+        XCTAssertEqual(urlComponents?.path, "/v2/url")
+    }
+}
 
+extension StarAPI_TestTests {
+    
+    class MockURLSession: SessionProtocol {
+        var url: URL?
+
+        func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            self.url = url
+            return URLSession.shared.dataTask(with: url)
+        }
+    }
 }
